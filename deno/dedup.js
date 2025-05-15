f24b12041d97f40b3cc9fa440dc2003555c167db
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-05-15 13:41:59 +0000

    Added a forced whitelist.

--- deno/dedup.js
+++ deno/dedup.js
@@ -10,67 +10,93 @@ let TreeNode = class TreeNode {
 	children = new Map();
 };
 
+let ignoreExact = new Map();
+
 for (let target in instanceConfig) {
-	// Define a root node
-	let rootNode = new TreeNode();
-	// Build a tree
-	for (let sourceId of instanceConfig[target]) {
-		let sourceFileHandle = await Deno.open(`./intermediate/${sourceId}`);
-		let lineNo = 0;
-		for await (let line of TextReader.line(sourceFileHandle.readable)) {
-			//console.debug(`${sourceId} :: ${line}`);
-			lineNo ++;
-			if (line.indexOf("domain:") === 0) {
-				let domainArray = line.substring(7).split(".").reverse();
-				//console.debug(domainArray);
-				let focusedNode = rootNode;
-				for (let i = 0; i < domainArray.length; i ++) {
-					let nibble = domainArray[i];
-					if (!focusedNode.children.has(nibble)) {
-						let newNode = new TreeNode();
-						newNode.data = domainArray.slice(0, i + 1);
-						focusedNode.children.set(nibble, newNode);
+	switch (target) {
+		case ":ignore-exact": {
+			for (let sourceId of instanceConfig[target]) {
+				let sourceFileHandle = await Deno.open(`./intermediate/${sourceId}`);
+				let lineNo = 0;
+				for await (let line of TextReader.line(sourceFileHandle.readable)) {
+					lineNo ++;
+					if (line.indexOf("domain:") === 0) {
+						ignoreExact.set(line.substring(7), true);
+					} else {
+						console.debug(`Ignore Exact Match: "${sourceId}", line ${lineNo} ignored.`);
 					};
-					focusedNode = focusedNode.children.get(nibble);
 				};
-				focusedNode.terminate = true;
-			} else {
-				console.debug(`"${target}": "${sourceId}", line ${lineNo} ignored.`);
 			};
+			break;
 		};
-		console.debug(`"${target}": "${sourceId}", read ${lineNo} line(s).`);
-	};
-	//console.debug(rootNode);
-	// Traverse the tree and emit
-	let resultArray = [];
-	let encoderStream = new TextEncoderStream();
-	let encoderStreamWriter = encoderStream.writable.getWriter();
-	let targetFileHandle = await Deno.open(`./data/${target}`, {
-		write: true, truncate: true, create: true
-	});
-	encoderStream.readable.pipeTo(targetFileHandle.writable);
-	let iterateOneLevel = async (node) => {
-		if (node.terminate) {
-			return;
-		};
-		for (let kvPair of node.children) {
-			if (kvPair[1].terminate) {
-				//console.debug(kvPair[1].data);
-				resultArray.push(kvPair[1].data.reverse().join("."));
-			} else {
-				await iterateOneLevel(kvPair[1]);
+		default: {
+			// Define a root node
+			let rootNode = new TreeNode();
+			// Build a tree
+			for (let sourceId of instanceConfig[target]) {
+				let sourceFileHandle = await Deno.open(`./intermediate/${sourceId}`);
+				let lineNo = 0;
+				for await (let line of TextReader.line(sourceFileHandle.readable)) {
+					//console.debug(`${sourceId} :: ${line}`);
+					lineNo ++;
+					if (line.indexOf("domain:") === 0) {
+						let domainSlice = line.substring(7);
+						let domainArray = domainSlice.split(".").reverse();
+						if (ignoreExact.has(domainSlice)) {
+							console.debug(`"${target}": "${sourceId}", line ${lineNo} ignored with exact match.`);
+							continue;
+						};
+						//console.debug(domainArray);
+						let focusedNode = rootNode;
+						for (let i = 0; i < domainArray.length; i ++) {
+							let nibble = domainArray[i];
+							if (!focusedNode.children.has(nibble)) {
+								let newNode = new TreeNode();
+								newNode.data = domainArray.slice(0, i + 1);
+								focusedNode.children.set(nibble, newNode);
+							};
+							focusedNode = focusedNode.children.get(nibble);
+						};
+						focusedNode.terminate = true;
+					} else {
+						console.debug(`"${target}": "${sourceId}", line ${lineNo} ignored.`);
+					};
+				};
+				console.debug(`"${target}": "${sourceId}", read ${lineNo} line(s).`);
 			};
+			//console.debug(rootNode);
+			// Traverse the tree and emit
+			let resultArray = [];
+			let encoderStream = new TextEncoderStream();
+			let encoderStreamWriter = encoderStream.writable.getWriter();
+			let targetFileHandle = await Deno.open(`./data/${target}`, {
+				write: true, truncate: true, create: true
+			});
+			encoderStream.readable.pipeTo(targetFileHandle.writable);
+			let iterateOneLevel = async (node) => {
+				if (node.terminate) {
+					return;
+				};
+				for (let kvPair of node.children) {
+					if (kvPair[1].terminate) {
+						//console.debug(kvPair[1].data);
+						resultArray.push(kvPair[1].data.reverse().join("."));
+					} else {
+						await iterateOneLevel(kvPair[1]);
+					};
+				};
+			};
+			await iterateOneLevel(rootNode);
+			resultArray.sort();
+			encoderStreamWriter.write(`# Deduplicated.\n`);
+			for (let line of resultArray) {
+				await encoderStreamWriter.ready;
+				encoderStreamWriter.write(`domain:${line}\n`);
+			};
+			console.debug(`"${target}": wrote ${resultArray.length} line(s).`);
+			encoderStreamWriter.close();
+			await encoderStreamWriter.closed;
+			targetFileHandle.close();
 		};
 	};
-	await iterateOneLevel(rootNode);
-	resultArray.sort();
-	encoderStreamWriter.write(`# Deduplicated.\n`);
-	for (let line of resultArray) {
-		await encoderStreamWriter.ready;
-		encoderStreamWriter.write(`domain:${line}\n`);
-	};
-	console.debug(`"${target}": wrote ${resultArray.length} line(s).`);
-	encoderStreamWriter.close();
-	await encoderStreamWriter.closed;
-	targetFileHandle.close();
 };
